c77968d24079c94baae0a20c37eeb59c177f65d2
aakash19here <spacex3nasa@gmail.com>
2023-06-16 20:47:21 +0530

    chore: Small Fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import { outfit, poppins } from "../utils/font";
 export default function Card() {
   return (
     <div
-      className={`${outfit.className} bg-[#041220] max-w-xs lg:max-w-xl mx-auto h-auto mt-[5%] rounded-md shadow-md lg:shadow-lg shadow-black pb-10 lg:pb-14`}
+      className={`${outfit.className} bg-[#041220] max-w-xs lg:max-w-xl mx-auto h-auto mt-[5%] rounded-md shadow-black shadow-lg pb-10 lg:pb-14`}
     >
       <div
         className={`text-center px-2 lg:px-10 pt-10 flex justify-center text-white text-2xl lg:text-4xl`}
@@ -24,7 +24,7 @@ export default function Card() {
         <p
           className={`${poppins.className} text-slate-400 text-xs lg:text-sm p-5 font-extralight`}
         >
-          Our team at Neural Nexus has years of experience shaping the future of
+          Our team at NexaLabs has years of experience shaping the future of
           both emerging startups and established companies through intelligent
           design and AI innovation. Every project we undertake is approached
           with the same level of excellence and commitment, always.
--- src/components/Product.js
+++ src/components/Product.js
@@ -19,6 +19,8 @@ export default function Product() {
         ))}
       </div>
       <div className="hidden lg:block border-r-2 border-gray-200"></div>
+      <div className="block lg:hidden mr-8 border-t-2 border-gray-200"></div>
+
       <div className="flex flex-col justify-center mt-3 mb-10">
         <h1 className="text-start text-3xl font-light lg:border-b-2 border-gray-200 pb-2">
           App Design <br /> & Dev
